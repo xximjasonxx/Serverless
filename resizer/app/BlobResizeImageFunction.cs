1975acde171fd0b106cf024f45e9c3b0a851eb74
Jason Farrell <imjason@gmail.com>
2022-11-05 08:26:13 -0400

    updated resizing logic

--- resizer/app/BlobResizeImageFunction.cs
+++ resizer/app/BlobResizeImageFunction.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
@@ -10,7 +12,7 @@ namespace ImageApi
     public class BlobResizeImageFunction
     {
         [FunctionName("BlobResizeImageFunction")]
-        public void Run(
+        public async Task Run(
             [BlobTrigger("raw/{blobName}", Connection = "StorageAccountConnection")]Stream rawBlob,
             string blobName,
             [Blob("resized/{blobName}", FileAccess.Write, Connection = "StorageAccountConnection")]Stream resizeBlob,
@@ -18,13 +20,18 @@ namespace ImageApi
         {
             log.LogInformation($"C# Blob trigger function Processing blob\n Name:{blobName} \n Size: {rawBlob.Length} Bytes");
 
-            IImageFormat format;
-            using Image image = Image.Load(rawBlob, out format);
-            int width = image.Width / 2;
-            int height = image.Height / 2;
+            using (var memStream = new MemoryStream())
+            {
+                IImageFormat format;
+                using (var image = Image.Load(rawBlob, out format))
+                {
+                    var newWidth = (int)Math.Round(image.Width * .25m);
+                    var newHeight = (int)Math.Round(image.Height * .25m);
 
-            image.Mutate(x => x.Resize(width, height));
-            image.SaveAsync(resizeBlob, format);
+                    image.Mutate(c => c.Resize(newWidth, newHeight, KnownResamplers.Lanczos3));
+                    await image.SaveAsync(resizeBlob, format);
+                }
+            }
 
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{blobName} \n Size: {resizeBlob.Length} Bytes");
         }
